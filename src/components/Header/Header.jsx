e825bd7c81169abaabf4791501c25593a25e37c0
akashseub <miahmdakash55@gmail.com>
2024-05-28 11:41:40 +0600

    try to finish this

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,63 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom';
-import logo from '../../assets/eme-logo.png'
+import { Link } from "react-router-dom";
+import logo from "../../assets/eme-logo.png";
 
-const style = {
-
-}
+const style = {};
 
 const Header = ({ loginButtonHandler }) => {
-    return (
-        <header className=' px-10 py-4 shadow-md'>
-            <div className='flex justify-between items-center rounded-xl bg-slate-100 px-10 py-4'>
-                <div className='flex items-center space-x-4'>
-                    <img className='w-12' src={logo} alt="" />
-                    <h1 className="text-4xl font-bold"><span className='text-[#182C61]'>EME</span><span className='text-[#2C3A47]'>ISLTD</span></h1>
-                </div>
-                <div className='flex space-x-8 items-center'>
-                    <ul className='flex space-x-4 font-semibold'>
-                        <Link className='hover:text-[#2C3A47]' to='/'>Home</Link>
-                        <Link className='hover:text-[#2C3A47]' to='/service'>Service</Link>
-                        <Link className='hover:text-[#2C3A47]' to='/about'>About Us</Link>
-                        <Link className='hover:text-[#2C3A47]' to='/contact'>Contact Us</Link>
-                    </ul>
-                    <button id='login-button' onClick={() => document.getElementById('my_modal_5').showModal()} className='bg-[#182C61] text-white font-extrabold px-4 py-2 rounded-lg'>Login</button>
-                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <div className="modal-action">
-                                <form method="dialog">
-                                    <button className="btn items-center">Login</button>
-                                </form>
-                            </div>
-                        </div>
-                    </dialog>
-                </div>
+  return (
+    <header className="shadow-md bg-slate-100 ">
+      <div className="flex justify-between rounded-xl max-w-screen-xl mx-auto py-2">
+        <div className="flex items-center space-x-4">
+          <img className="w-12" src={logo} alt="" />
+          <h1 className="text-4xl font-bold">
+            <span className="text-[#182C61]">EME</span>
+            <span className="text-[#2C3A47]">ISLTD</span>
+          </h1>
+        </div>
+        <div className="flex space-x-8 items-center">
+          <ul className="flex space-x-4 font-semibold">
+            <Link className="hover:text-[#2C3A47]" to="/">
+              Home
+            </Link>
+            <Link className="hover:text-[#2C3A47]" to="/service">
+              Service
+            </Link>
+            <Link className="hover:text-[#2C3A47]" to="/about">
+              About Us
+            </Link>
+            <Link className="hover:text-[#2C3A47]" to="/contact">
+              Contact Us
+            </Link>
+          </ul>
+          <button
+            id="login-button"
+            onClick={() => document.getElementById("my_modal_5").showModal()}
+            className="bg-[#182C61] text-white font-extrabold px-4 py-2 rounded-lg"
+          >
+            Login
+          </button>
+          <dialog
+            id="my_modal_5"
+            className="modal modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box">
+              <h3 className="font-bold text-lg">Hello!</h3>
+              <p className="py-4">
+                Press ESC key or click the button below to close
+              </p>
+              <div className="modal-action">
+                <form method="dialog">
+                  <button className="btn items-center">Login</button>
+                </form>
+              </div>
             </div>
-        </header>
-    );
+          </dialog>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/SlideShow/SlideShow.jsx
+++ src/components/SlideShow/SlideShow.jsx
@@ -1,70 +1,92 @@
-import image1 from '../../assets/1.png'
-import image2 from '../../assets/2.png'
-import image3 from '../../assets/3.png'
-import image4 from '../../assets/4.png'
+import image1 from "../../assets/1.png";
+import image2 from "../../assets/2.png";
+import image3 from "../../assets/3.png";
+import image4 from "../../assets/4.png";
 
 const SlideShow = () => {
-    return (
-        <div>
-            {/* <div>
-                <div className="carousel w-full h-96">
-                    <div id="item1" className="carousel-item w-full">
-                        <img src={image1} className="w-full" />
-                    </div>
-                    <div id="item2" className="carousel-item w-full">
-                        <img src={image2} className="w-full" />
-                    </div>
-                    <div id="item3" className="carousel-item w-full">
-                        <img src={image3} className="w-full" />
-                    </div>
-                    <div id="item4" className="carousel-item w-full">
-                        <img src={image4} className="w-full" />
-                    </div>
-                </div>
-                <div className="flex justify-center w-full py-2 gap-2">
-                    <a href="#item1" className="btn btn-xs">1</a>
-                    <a href="#item2" className="btn btn-xs">2</a>
-                    <a href="#item3" className="btn btn-xs">3</a>
-                    <a href="#item4" className="btn btn-xs">4</a>
-                </div>
-            </div> */}
-
-
-            <div>
-                <div className="carousel w-full h-[700px] bg-cover">
-                    <div id="slide1" className="carousel-item relative w-full">
-                        <img src={image1} className="w-full object-cover" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide4" className="btn btn-circle">❮</a>
-                            <a href="#slide2" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                    <div id="slide2" className="carousel-item relative w-full">
-                        <img src={image2} className="w-full object-cover" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide1" className="btn btn-circle">❮</a>
-                            <a href="#slide3" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                    <div id="slide3" className="carousel-item relative w-full">
-                        <img src={image3} className="w-full object-cover" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide2" className="btn btn-circle">❮</a>
-                            <a href="#slide4" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                    <div id="slide4" className="carousel-item relative w-full">
-                        <img src={image4} className="w-full object-cover" />
-                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                            <a href="#slide3" className="btn btn-circle">❮</a>
-                            <a href="#slide1" className="btn btn-circle">❯</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      {/* <div>
+        <div className="carousel w-full h-96">
+          <div id="item1" className="carousel-item w-full">
+            <img src={image1} className="w-full" />
+          </div>
+          <div id="item2" className="carousel-item w-full">
+            <img src={image2} className="w-full" />
+          </div>
+          <div id="item3" className="carousel-item w-full">
+            <img src={image3} className="w-full" />
+          </div>
+          <div id="item4" className="carousel-item w-full">
+            <img src={image4} className="w-full" />
+          </div>
         </div>
-    );
+        <div className="flex justify-center w-full py-2 gap-2">
+          <a href="#item1" className="btn btn-xs">
+            1
+          </a>
+          <a href="#item2" className="btn btn-xs">
+            2
+          </a>
+          <a href="#item3" className="btn btn-xs">
+            3
+          </a>
+          <a href="#item4" className="btn btn-xs">
+            4
+          </a>
+        </div>
+      </div> */}
+
+      <div>
+        <div className="carousel max-w-screen-xl h-[700px] bg-cover">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img src={image1} className="w-full object-cover" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img src={image2} className="w-full object-cover" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full">
+            <img src={image3} className="w-full object-cover" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img src={image4} className="w-full object-cover" />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SlideShow;
-
