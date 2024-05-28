705c781525cf983bf275e48e88db7b2c0315e006
akashseub <miahmdakash55@gmail.com>
2024-05-28 11:42:45 +0600

    I will finish this job

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 // import LoginForm from "../LoginForm/LoginForm";
-import SlideShow from "../SlideShow/SlideShow";
+// import SlideShow from "../SlideShow/SlideShow";
 
 const loginButtonHandler = () => {
-    console.log('button clicked')
-}
+  console.log("button clicked");
+};
 
 const Home = () => {
-    return (
-        <div className="">
-            <Header loginButtonHandler={loginButtonHandler}></Header>
-            <SlideShow></SlideShow>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="">
+      <Header loginButtonHandler={loginButtonHandler}></Header>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Home;
